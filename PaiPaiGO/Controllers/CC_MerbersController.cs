a3c024008666e478bb824793abb9321ad4410919
WS1801\User <catyang41@gmail.com>
2023-10-17 16:57:21 +0800

    1017_17:00 使用SweetAlert2套件+修改完成

--- PaiPaiGO/Controllers/CC_MerbersController.cs
+++ PaiPaiGO/Controllers/CC_MerbersController.cs
@@ -216,7 +216,7 @@ namespace paipaigo1005.Controllers {
                 numstring = "0" + numstring;
             }
             member.MemberId = numstring;
-            member.MemberStatus = "正常";
+            //member.MemberStatus = "正常";
 
 			if (ModelState.IsValid) {
                 (string hashedPassword, string salt) = PasswordHasher.HashPassword(member.MemberPassword);
@@ -226,11 +226,11 @@ namespace paipaigo1005.Controllers {
                 _context.Add(member);
                 await _context.SaveChangesAsync();
 
-                //發送電子郵件
-                var emailSubject = "paipaigo會員驗證郵件";
-                var emailMessage = $"請點擊連結驗證您的Email地址以完成註冊會員：<a href='{Url.Action("ConfirmEmail", "CC_Members", new { memberId = member.MemberId, token = GenerateToken(member.MemberId) }, protocol: HttpContext.Request.Scheme)}'>驗證並至登入頁面</a>。<br>此為系統郵件，請勿回復。<br>PaiPaiGO";
-                var emailService = new EmailService();
-                await emailService.SendEmailAsync(member.MemberEmail, emailSubject, emailMessage);
+                ////發送電子郵件
+                //var emailSubject = "paipaigo會員驗證郵件";
+                //var emailMessage = $"請點擊連結驗證您的Email地址以完成註冊會員：<a href='{Url.Action("ConfirmEmail", "CC_Members", new { memberId = member.MemberId, token = GenerateToken(member.MemberId) }, protocol: HttpContext.Request.Scheme)}'>驗證並至登入頁面</a>。<br>此為系統郵件，請勿回復。<br>PaiPaiGO";
+                //var emailService = new EmailService();
+                //await emailService.SendEmailAsync(member.MemberEmail, emailSubject, emailMessage);
                 ViewBag.RegistrationSuccess = "Success";
                 return View("Resgister");
             }
